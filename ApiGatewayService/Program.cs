a1aeb16b4c1312b0905b4ae116146086c8eedf32
Fernando Chávez <fpatric25@gmail.com>
2025-11-16 14:35:34 -0300

    feat(gateway): configure http client for auth-service and add jwt authentication

--- ApiGatewayService/Program.cs
+++ ApiGatewayService/Program.cs
@@ -2,8 +2,9 @@ using System.Reflection;
 using ApiGatewayService.Filters;
 using ApiGatewayService.Services.Implementation;
 using ApiGatewayService.Services.Interfaces;
-using Clients;
-
+using Microsoft.AspNetCore.Authentication.JwtBearer; 
+using Microsoft.IdentityModel.Tokens; 
+using System.IdentityModel.Tokens.Jwt;
 var builder = WebApplication.CreateBuilder(args);
 
 
@@ -30,6 +31,37 @@ builder.Services.AddGrpcClient<Clients.Clients.ClientsClient>(o =>
     };
     return handler;
 });
+builder.Services.AddHttpClient("AuthServiceClient", client =>
+{
+    var url = builder.Configuration["ServiceUrls:AuthService"];
+    client.BaseAddress = new Uri(url!);
+})
+.ConfigurePrimaryHttpMessageHandler(() =>
+{
+    var handler = new HttpClientHandler();
+    handler.ServerCertificateCustomValidationCallback = 
+        HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+    return handler;
+});
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = false,
+        ValidateIssuerSigningKey = false,
+        SignatureValidator = (token, parameters) => new JwtSecurityToken(token) 
+    };
+});
+
+builder.Services.AddAuthorization();
 builder.Services.AddScoped<IClientService, ClientService>();
 builder.Services.AddEndpointsApiExplorer();
 var app = builder.Build();
@@ -37,6 +69,7 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
